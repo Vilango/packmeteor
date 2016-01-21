18baf14c26992f997c55c4b63186a31023b23bae
Gerwin Brunner <gerwin.brunner@gmail.com>
2016-01-21 15:08:37 +0100

    updated package

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'gbrunner:packmeteor',
-  version: '0.1.10',
+  version: '0.1.11',
   summary: "Rig the basics for packmeteor",
   git: 'https://github.com/Vilango/packmeteor'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@0.9.4');
+  api.versionsFrom('METEOR@1.1.0.3');
   api.use('webapp', 'server');
   api.use('reload', 'client');
   api.use('routepolicy', 'server');
